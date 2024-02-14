ec98d8398ffc20caefc4c2093d3c818e3b5c1ba4
eren <eren_bekmezci@hotmail.com.tr>
2024-02-14 17:48:49 +0300

    front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+import './styles/Chat.css'; 
+
+const Chat = () => {
+    const [sender, setSender] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [typing, setTyping] = useState('');
+
+    useEffect(() => {
+        const socket = io.connect('http://localhost:3000');
+
+        socket.on('chat', data => {
+            setMessages(prevMessages => [...prevMessages, { sender: data.sender, message: data.message }]);
+        });
+
+        socket.on('typing', data => {
+            setTyping(`${data} typing...`);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const handleSubmit = () => {
+        const socket = io.connect('http://localhost:3000');
+        socket.emit('chat', { message, sender });
+        setMessage('');
+    };
+
+    const handleTyping = () => {
+        const socket = io.connect('http://localhost:3000');
+        socket.emit('typing', sender);
+    };
+
+    return (
+        <div id="chat-wrap">
+            <h2>Sohbet</h2>
+            <div id="chat-window">
+                {messages.map((msg, index) => (
+                    <div key={index}>
+                        <p><strong>{msg.sender} : </strong>{msg.message}</p>
+                    </div>
+                ))}
+            </div>
+            <input type="text" id="sender" placeholder="İsim" value={sender} onChange={e => setSender(e.target.value)} />
+            <input type="text" id="message" placeholder="Mesaj Gir" value={message} onChange={e => setMessage(e.target.value)} onKeyPress={handleTyping} />
+            <button id="submitBtn" onClick={handleSubmit}>Gönder</button>
+            <div id="feedback">
+                <p>{typing}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
